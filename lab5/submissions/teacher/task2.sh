2a3202380b90c2900403b44ed48f00b65dd62891
LeeWilli <wang.li.neu@gmail.com>
2026-05-14 00:29:28 +0800

    update lab5

--- lab5/submissions/teacher/task2.sh
+++ lab5/submissions/teacher/task2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while IFS= read -r cmd || [ -n "$cmd" ]; do
     if [ -z "$cmd" ]; then continue; fi
-    ni=$(ps -C "$cmd" -o ni= --sort=pid | head -n 1 | tr -d ' ')
+    ni=$(ps -C "$cmd" -o ni= --sort=pid | head -n 1 | tr -d ' ' || true)
     if [ -n "$ni" ]; then
         echo "$cmd $ni"
     else
